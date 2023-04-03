07b4f68f4104f7b7472833e5de85dc5b3b5d4072
DanielMurciaPerdomo <danielmurciap@gmail.com>
2023-04-02 20:17:30 -0500

    app terminada

---
+++ index.js
@@ -0,0 +1,70 @@
+const container = document.getElementById("contenedor");
+const search = document.getElementById("boton");
+const weatherBox = document.getElementById("clima");
+const weatherDetails = document.getElementById("detalles");
+const error404 = document.getElementById("error");
+
+search.addEventListener("click", () => {
+  const APIKey = "a79b9738c176f9d78b04ac250ea0809d";
+  const city = document.querySelector(".search-box input").value;
+
+  if (city === "") return;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.cod === "404") {
+        container.style.height = "400px";
+        weatherBox.style.display = "none";
+        weatherDetails.style.display = "none";
+        error404.style.display = "block";
+        error404.classList.add("fadeIn");
+        return;
+      }
+
+      error404.style.display = "none";
+      error404.classList.remove("fadeIn");
+
+      const image = document.getElementById("foto");
+      const temperature = document.getElementById("temperatura");
+      const description = document.getElementById("descripcion");
+      const humidity = document.getElementById("humedad");
+      const wind = document.getElementById("viento");
+
+      switch (json.weather[0].main) {
+        case "Clear":
+          image.src = "images/clear.png";
+          break;
+
+        case "Rain":
+          image.src = "images/rain.png";
+          break;
+
+        case "Snow":
+          image.src = "images/snow.png";
+          break;
+
+        case "Clouds":
+          image.src = "images/clouds.png";
+          break;
+
+        case "Haze":
+          image.src = "images/haze.png";
+          break;
+
+        default:
+          image.src = "";
+      }
+
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+      weatherBox.style.display = "";
+      weatherDetails.style.display = "";
+      weatherBox.classList.add("fadeIn");
+      weatherDetails.classList.add("fadeIn");
+      container.style.height = "590px";
+    });
+});
